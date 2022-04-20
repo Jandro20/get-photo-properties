184f3d5ee6acc78c52c8b6b1455c729c42fef1fe
ahmrivera <ahmrivera@gmail.com>
2022-04-20 21:32:19 +0200

    Added option to specify the type of the image

--- get_properties.py
+++ get_properties.py
@@ -3,14 +3,24 @@
 import os
 import sys
 import getopt
+from datetime import datetime
 from PIL import Image
 from PIL.ExifTags import TAGS
 
-def get_set_of_images(path):
+def get_set_of_images(path, type_img):
     """ Get the list of images that we will use it """
 
-    images = list(filter(lambda k: '.jpg' in k, os.listdir(path)))
-    get_metadata(images)
+    if type_img != "":
+        images = list(filter(lambda x: type_img in x, os.listdir(path)))
+    else:
+        images = list(os.listdir(path))
+
+    if images:
+        get_metadata(images)
+    else:
+        print("Empty list of images! Check image folder.")
+        exit(1)
+
 
 def get_metadata(images):
     """ Obtain the metadata from images (list) and write it in LOG file"""
@@ -46,9 +56,10 @@ def get_metadata(images):
 if __name__ == "__main__":
 
     PATH = ""
+    TYPE = ""
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:", ["output="])
+        opts, args = getopt.getopt(sys.argv[1:], "ho:t:", ["output=", "type="])
     except getopt.GetoptError:
         print("Command to use this tool: python get_properties.py -o <path/to/output/file>")
         sys.exit(2)
@@ -58,13 +69,25 @@ if __name__ == "__main__":
             print("""You can use this command with some options:
     - If you use it with no options, the result will be saved in the same path where the tool is saved.
     - If you specify the output path, "python get_properties.py -o <path>", the result will be saved in <path>self.
-    - If you need see this help again, "python get_properties.py -h".""")
+    - If you need see this help again, "python get_properties.py -h".
+    - If you want specify the type of image to get properties, "python get_properties.py -t
+        ["jpg", "jpeg", "png", "raw"]" """)
             sys.exit(0)
         elif opt in ("-o", "--output"):
             PATH = arg
+        elif opt in ("-t", "--type"):
+            if arg in ["jpg", "jpeg", "png", "raw"]:
+                TYPE = str(arg)
+            else:
+                print("Specified type of image is not supported")
+                sys.exit(1)
 
+    # If path is not specified, we will use the path of the tool dir
     if PATH == "":
         PATH = os.path.dirname(os.path.abspath(__file__))
+
+    if TYPE == "":
+        print("Parsing all images in ./images")
 
     if not os.path.exists(PATH):
         print(f"Path selected is not a valid path [{PATH}]")
@@ -72,9 +95,9 @@ if __name__ == "__main__":
 
     print(f"Path selected is: {PATH}")
 
-    file = PATH+"/result.txt"
+    file = PATH+"/result-"+ datetime.now().strftime("%H%M%S") +".txt"
     LOG = open(file, 'w', encoding='utf-8')
-    get_set_of_images("./images/")
+    get_set_of_images("./images/", TYPE)
     LOG.close()
 
     print(f"Completed! File created in {PATH}/result.txt")
